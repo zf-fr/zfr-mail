cfd4cb189d5d050e5cf6697282d0b25c72c7bde1
Michaël Gallego <mic.gallego@gmail.com>
2016-02-27 10:32:15 +0100

    Set interfaces

---
+++ src/Mail/MailInterface.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace ZfrMail\Mail;
+
+/**
+ * Simple interface for a mail
+ * 
+ * @author Michaël Gallego
+ */
+interface MailInterface
+{
+    /**
+     * Add a new "from" address and return a new mail
+     *
+     * @param  string $from
+     * @return MailInterface
+     */
+    public function withFrom(string $from): MailInterface;
+
+    /**
+     * Get the "from" email
+     *
+     * @return string
+     */
+    public function getFrom(): string;
+
+    /**
+     * Add a new "to" address and return a new mail
+     *
+     * @param  string $to
+     * @return MailInterface
+     */
+    public function withTo(string $to): MailInterface;
+
+    /**
+     * Get the "to" email
+     *
+     * @return string
+     */
+    public function getTo(): string;
+
+    /**
+     * Add a new "cc" addresses and return a new mail
+     *
+     * @param  array $cc
+     * @return MailInterface
+     */
+    public function withCc(array $cc): MailInterface;
+
+    /**
+     * Get the "cc" emails
+     *
+     * @return array
+     */
+    public function getCc(): array;
+
+    /**
+     * Add a new "bcc" addresses and return a new mail
+     *
+     * @param  array $bcc
+     * @return MailInterface
+     */
+    public function withBcc(array $bcc): MailInterface;
+
+    /**
+     * Get the "bcc" emails
+     *
+     * @return array
+     */
+    public function getBcc(): array;
+
+    /**
+     * Set new options (the options are related to the mailer you use)
+     *
+     * @param  array $options
+     * @return MailInterface
+     */
+    public function withOptions(array $options): MailInterface;
+
+    /**
+     * Get the options
+     *
+     * @return array
+     */
+    public function getOptions(): array;
+}
